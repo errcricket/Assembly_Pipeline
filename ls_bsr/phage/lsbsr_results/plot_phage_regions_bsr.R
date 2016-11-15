53d0af5d6173c09d0b15d173cde012d76cd6c6dd
errcricket <work.err29@gmail.com>
2016-11-15 13:05:19 -0500

    PHASTER output analyzed

---
+++ ls_bsr/phage/lsbsr_results/plot_phage_regions_bsr.R
@@ -0,0 +1,67 @@
+#!/usr/bin/Rscript 
+
+options(warn=1)
+
+library(ggplot2)
+library(gplots)
+library(readr)
+library(reshape2)
+library(RColorBrewer)
+library(wesanderson)
+library(colorRamps)
+library(ggdendro)
+
+original.parameters=par()
+options(width=9999)
+
+args = commandArgs(trailingOnly=TRUE)
+
+file_name <- args[1]
+image_name <- args[2]
+base  <- args[3]
+#file_name_2 <- args[3]
+#image_name_2 <- args[4]
+
+data <- read_delim(file_name, delim='\t')
+names(data)[1] <- 'Gene'
+
+
+m_data <- melt(data)
+names(m_data)[2] <- 'Strain'
+names(m_data)[3] <- 'LS_BSR'
+m_data <- m_data[ which(m_data$Strain != 'B055'),]
+
+write.table(m_data, 'temp.txt', sep = '\t', row.names = F, col.names = T, quote = F)
+
+col_breaks = c(0, 0.5, 1)
+my_palette <- colorRampPalette(wes_palette('Zissou', 21, type = 'continuous'))
+# Zissou = c("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"),
+title_name = paste('LS-BSR for Phage Region', base)
+
+png(filename=image_name, width=3750,height=2750,res=300)
+print(i <- ggplot(m_data, aes(x=Gene, y=Strain)) +
+		geom_tile(color='gray', aes(fill = LS_BSR)) + 
+		theme(axis.text.x=element_text(angle=90, hjust=1, size=7), axis.title.x=element_text(size=14))  +
+		theme(axis.text.y=element_text(size=9), axis.title.y=element_text(size=14))  +
+		#labs(title='LS-BSR for Phage Region') + 
+		labs(title=title_name) + 
+		theme(plot.title = element_text(size=18)) +
+		scale_fill_gradientn(colours = my_palette(300)) )
+dev.off()
+#
+#data_g <- read_delim(file_name_2, delim='\t')
+#names(data_g)[1] <- 'Gene'
+#
+#m_data_g <- melt(data_g)
+#names(m_data_g)[2] <- 'Strain'
+#names(m_data_g)[3] <- 'LS_BSR'
+#
+#png(filename=image_name_2, width=3750,height=2750,res=300) 
+#print(i <- ggplot(m_data_g, aes(x=Gene, y=Strain)) +
+#		geom_tile(color='gray', aes(fill = LS_BSR)) + 
+#		theme(axis.text.x=element_text(angle=90, hjust=1, size=7), axis.title.x=element_text(size=14))  +
+#		theme(axis.text.y=element_text(size=9), axis.title.y=element_text(size=14))  +
+#		labs(title='LS-BSR for Virulent Genes (by gene)') + 
+#		theme(plot.title = element_text(size=18)) +
+#		scale_fill_gradientn(colours = my_palette(300)) )
+#dev.off()
